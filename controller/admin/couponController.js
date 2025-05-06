0dd255eb4b7ce379d34ec891d4e5bcab23fe036a
Thanha Mariyam <thanhathanha665@gmail.com>
2025-05-06 10:33:00 +0530

    coupon pagination

--- controller/admin/couponController.js
+++ controller/admin/couponController.js
@@ -3,7 +3,7 @@ const httpStatus = require("../../utils/httpStatus");
 const getCoupon = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = 4;
+    const limit = 8;
     const skip = (page - 1) * limit;
     const q = req.query.q || "";
     const regex = new RegExp(q, "i");
